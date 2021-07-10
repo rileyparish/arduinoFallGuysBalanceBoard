fb0028788badaaaf85e842252f8fbf98728bd819
riley <parish.riley@gmail.com>
2021-07-10 17:19:33 -0600

    The wasd keys get activated when the accelerometer is in the right range.

--- fallGuysBalance.ino
+++ fallGuysBalance.ino
@@ -1,63 +1,92 @@
-// (c) Michael Schoeffler 2017, http://www.mschoeffler.de
-
 #include "Wire.h" // This library allows you to communicate with I2C devices.
 #include <Keyboard.h>
 
 const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
 
 float curX, curY, curZ; // variables for accelerometer raw data
-float roll,pitch,rollF,pitchF=0;
+float roll,pitch,curRoll,curPitch=0;
 
-float tolerance = 2;        // how many degrees of tolerance to register an input (so the movement doesn't have to be as precise)
+const int numKeys = 4;
+
+const char keyCodes[numKeys] = {'w', 'a', 's','d'};
+const float rollThresholds[numKeys] = {0, 20, -4, -23};
+const float pitchThresholds[numKeys] = {-28, -1, 14, -9};
+
+
+float tolerance = 4.0;        // how many degrees of tolerance to register an input (so the movement doesn't have to be as precise)
 // every character/key has these attributes
 struct AccelInput {
     char keycode;
-    float pitch;
     float roll;
-    boolean wasPressed = false;
+    float pitch;
+    boolean wasActive = false;
 };
 
 
+boolean shouldActivate = false;     // this keeps track of whether a given key should be pressed or not given the current readings
 
-// this is an array of TouchInput structs called "Pins" that contains 4 items in it (wasd)
-AccelInput Pins[4];
+// this is an array of AccelInput structs called "Inputs" that contains numKeys items in it (wasd)
+AccelInput Inputs[numKeys];
 
 
 void setup() {
     Serial.begin(115200);
+    // set up the accelerometer
     Wire.begin();
     Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
     Wire.write(0x6B); // PWR_MGMT_1 register
     Wire.write(0); // set to zero (wakes up the MPU-6050)
     Wire.endTransmission(true);
+
+    // initialize the wasd structs
+    for(int i = 0; i < numKeys; i++){
+        Inputs[i].keycode = keyCodes[i];
+        Inputs[i].roll = rollThresholds[i];
+        Inputs[i].pitch = pitchThresholds[i];
+    }
 }
 
 
 void loop() {
-    // this reads from the accelerometer and calculates the pitch and roll, which are stored in pitchF and rollF
-    getPitchRoll();
+    // this function reads from the accelerometer and calculates the roll and pitch, which are stored in curRoll and curPitch
+    getRollPitch();
 
-
-    
-
-
-    Serial.print(rollF);
-    Serial.print("/");
-    Serial.println(pitchF);
-
-    delay(10);
-
-    
+    // now determine whether each wasd key needs to be pressed or released
+    for(int i = 0; i < numKeys; i++){
+        shouldActivate = Inputs[i].wasActive;       // assume that the state will not change
+        // determine whether the key is within the range to register a press
+        if(isWithinRange(curRoll, Inputs[i].roll) && isWithinRange(curPitch, Inputs[i].pitch)){
+            // if the values are within range, the key should be pressed
+            shouldActivate = true;
+        }else{
+            shouldActivate = false;
+        }
+        
+        // if the activity state on this iteration is different from the previous iteration, press or release the key
+        if(shouldActivate != Inputs[i].wasActive){
+            if(shouldActivate){
+                 Keyboard.press(Inputs[i].keycode);
+            }else{
+                 Keyboard.release(Inputs[i].keycode);
+            }
+            Inputs[i].wasActive = shouldActivate;
+            Serial.println();
+        }
+    }
 }
 
-// reads from the accelerometer calculates the pitch and roll of the board
-void getPitchRoll(){
-    
+// is the current roll/pitch within range of the target values for a given key?
+bool isWithinRange(float curVal, float targetVal){
+    return ((curVal > targetVal - tolerance) && (curVal < targetVal + tolerance));
+}
+
+// reads from the accelerometer and calculate the roll and pitch of the board
+void getRollPitch(){
     // Read from the accelerometer to get the position of the balance board
     Wire.beginTransmission(MPU_ADDR);
     Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
     Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
-    Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
+    Wire.requestFrom(MPU_ADDR, 3*2, true); // used to be 7 * 2 but since I only need 3 pieces of data
     
     // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
     curX = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
@@ -72,42 +101,8 @@ void getPitchRoll(){
     roll = atan(curY / sqrt(pow(curX, 2) + pow(curZ, 2))) * 180 / PI;
     pitch = atan(-1 * curX / sqrt(pow(curY, 2) + pow(curZ, 2))) * 180 / PI;
   
-    // Low-pass filter
-    rollF = 0.94 * rollF + 0.06 * roll;
-    pitchF = 0.94 * pitchF + 0.06 * pitch;
+    // Low-pass filter (to reduce noise). This is really important because the sensors are pretty noisy
+    curRoll = 0.94 * curRoll + 0.06 * roll;
+    curPitch = 0.94 * curPitch + 0.06 * pitch;
 }
-
-
-/*
-If the circuit has been completed or if the balance board readings are within a given threshold, press the key
-Otherwise, release the key
-
-But I don't think I want to be sending "press" all the time because they'll be treated as separate presses.
-So I need to keep track of whether the key is active or not and only press or release when the state has changed.
-
-
-Data members:
-    active - this is true if the button is currently being pressed
-    keycode - 
-
-
-I do need to use the pair of coords to detect a given key press so I can be sure there's no overlap.
-
-
-If rollF ~ 0 && pitchF ~ -28
-    press w
-else
-    release w
-
-
-get accel data
-determine if the key has changed state from the last iteration
-if it has, press or release the associated key
-
-
-
-*/
-
-
-
 
